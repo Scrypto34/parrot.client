d92b9b009b761b5edfa9b0522e5b9e4612449ba5
Scrypto <279328339+Scrypto34@users.noreply.github.com>
2026-09-01 18:03:56 +0200

    FUCK 1 file is bigger than 25MB womp womp

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+namespace Parrot.client
+{
+    public class PluginInfo
+    {
+        public const string GUID = "org.scrypto.parrot.gorillatag.parrotmenu";
+        public const string Name = "parrot.client";
+        public const string Description = "Created by parrot and Scrypto";
+        public const string Version = "1.0.0";
+    }
+}
